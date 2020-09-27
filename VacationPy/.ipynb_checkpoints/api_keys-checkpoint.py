851b74a2a1868616268e0a8469aa2cfac589f0ac
Felipe Lopez <you@example.commr.felipelopez@gmail.com>
2020-09-27 02:53:23 +0000

    Finished VacationPy

--- VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -3,3 +3,6 @@ weather_api_key = "4707a73af6ec1e4ef8fb1abb32369013"
 
 # Google API Key
 g_key = "AIzaSyCsc7EmKDbotNSZRnyFcyGgbpEh17-J3Dw"
+
+# Mapbox Token
+mapbox_token = "pk.eyJ1IjoiZmxvcGV6anIiLCJhIjoiY2tmOXcwMjdrMDFhaDJ6andhYncyeDh5NCJ9.zLf6t52S7smD-WG23URv2A"
